42e86595b848998345efb556b9be3b25211328ef
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-10-29 17:36:00 -0700

    commit tree 114058d604588b548b5c949a5a6602f0e33a3442

---
+++ lib/user.rb
@@ -0,0 +1,5 @@
+class User #define the User class such that a user can have a first and last name. Need both a setter and a getter for first and last name.
+
+    attr_accessor :first_name , :last_name
+
+end
